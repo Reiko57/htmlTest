67fb8c73763f402f008399e1f3218e4b55d1beec
Reiko <reiko.yokoyama@rakuten-sec.co.jp>
2021-08-19 15:44:12 +0900

    first commit

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <meta charset="UTF-8" />
-    <title>tech boost</title>
-    <style>
-  h1 {
-    color: red;
-  }
-  </style>
+  <meta charset="UTF-8" />
+  <title>tech boost</title>
 </head>
-    <body>
-        <h1>Hello world!!!</h1>
-        <p>はじめてのwebサイトへようこそ</p>
-    </body>
+  <body>
+    <h1>Hello world!!!</h1>
+    <p>CSSの入門！</p>
+  </body>
 </html>
